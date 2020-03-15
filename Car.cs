e8a2067f834b0bcfe0210e53da2edd8395271e32
gaoqiaoxia <gaoqiaoxia@moretickets.com>
2020-03-15 23:37:34 +0800

    添加项目文件。

---
+++ Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Car
+    {
+        string _species;
+        string _color;
+        int _chair;
+        int _speed;
+
+        public string Species { get => _species; set => _species = value; }
+        public string Color { get => _color; set => _color = value; }
+        public int Chair { get => _chair; set => _chair = value; }
+        public int Speed { get => _speed; set => _speed = value; }
+
+        public string move()
+        {
+            string say="汽车停下来了";
+            return say;
+        }
+        public string move(int speed)
+        {
+            string say = "汽车开起来了";
+            return say;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Species = "路虎";
+            car.Color = "白色";
+            car.Chair = 8;
+            Console.WriteLine("车的类型：{0}", car.Species);
+            Console.WriteLine("车的颜色:{0}", car.Color);
+            Console.WriteLine("车的椅子数:{0}", car.Chair);
+            Console.WriteLine(car.move());
+            car.Speed = 100;
+            Console.WriteLine(car.move(car.Speed));
+
+        }
+    }
+}
