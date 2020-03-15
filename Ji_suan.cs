be95d590e273d15bcd5040ed07b304afac5edac0
gaoqiaoxia <gaoqiaoxia@moretickets.com>
2020-03-15 21:27:57 +0800

    first commit

---
+++ Ji_suan.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Ji_suan
+    {
+        float  z;
+       
+       
+        public float ji_suan(float x, float y, char f)
+        {
+
+            switch (f)
+            {
+                case '+':
+                    Console.WriteLine("两个数相加，结果为：");
+                    z = x + y;
+                    break;
+                case '-':
+                    Console.WriteLine("两个数相减，结果为：");
+                    z = x - y;
+                    break;
+                case '*':
+                    Console.WriteLine("两个数相乘，结果为：");
+                    z = x * y;
+                    break;
+                case '/':
+                    z = x / y;
+                    Console.WriteLine("两个数相除，结果为：");
+                    if (y == 0)
+                    {
+                        Console.WriteLine("分母为0无意义");
+                        break;
+                    }
+                    break;
+
+            }
+            return z;
+
+
+
+        }
+        public bool Equals(int x)
+        {
+            Console.WriteLine("只有一个参数不能进行比较");
+            return false;
+
+
+        }
+        public bool Equals(int x,int y)
+        {
+            if (x == y)
+            {
+                Console.WriteLine("两个数相等");
+                return true;
+
+            }
+            else
+            {
+                Console.WriteLine("两个数不相等");
+                return false;
+            }
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ji_suan ji = new Ji_suan();
+           
+            Console.WriteLine( ji.ji_suan(2, 3, '+'));
+            Console.WriteLine( ji.ji_suan(2, 3, '-'));
+            Console.WriteLine( ji.ji_suan(2, 3, '*'));
+            Console.WriteLine( ji.ji_suan(2, 3, '/'));
+            Console.WriteLine( ji.ji_suan(2, 0, '/'));
+
+            Console.WriteLine("Equals方法重载：{0}", ji.Equals(2));
+            Console.WriteLine("Equals方法重载：{0}", ji.Equals(2,3));
+
+        }
+    }
+}
